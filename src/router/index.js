afcb1718c18ccd17fcafa5a216aaec32b3dabbd1
Sohibjon <abduganiyevsoxib002@gmail.com>
2024-07-05 13:22:40 +0500

    refactor: changed error url addres to /users and changed title of project

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    {
-        path: '/users', 
-        name: 'users', 
-        component: () => import('@/views/Users.vue')
-    },
-    {
-        path: '/chat', 
-        name: 'chat', 
-        component: () => import('@/views/Chat.vue')
-    }
+  {
+    path: '/',
+    redirect: '/users'
+  },
+  {
+      path: '/users', 
+      name: 'users', 
+      component: () => import('@/views/Users.vue')
+  },
+  {
+      path: '/chat', 
+      name: 'chat', 
+      component: () => import('@/views/Chat.vue')
+  }
 ]
 
 const router = createRouter({
@@ -27,6 +31,8 @@ router.beforeEach((to, from, next) => {
     } else {
       next()
     }
+
+    document.title = 'Chat simulator';
   })
 
 export default router   
